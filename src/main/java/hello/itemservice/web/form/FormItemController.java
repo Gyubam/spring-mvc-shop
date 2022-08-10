9e116e34b58837f58dcc670d467d882dcbbd6e50
서규범 <sgb8170@naver.com>
2022-08-11 00:02:37 +0900

    상품 등록 시 검증 코드 추가

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -8,13 +8,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Slf4j
 @Controller
@@ -37,6 +35,7 @@ public class FormItemController {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
 
+        // 체크박스, 라디오버튼, 텍스트상자 설정 코드 -- start
         Map<String, String> regions = new LinkedHashMap<>();
         regions.put("SEOUL", "서울");
         regions.put("BUSAN", "부산");
@@ -51,6 +50,7 @@ public class FormItemController {
         deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
         deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
         model.addAttribute("deliveryCodes", deliveryCodes);
+        // 체크박스, 라디오버튼, 텍스트상자 설정 코드 -- end
 
         return "form/item";
     }
@@ -59,6 +59,7 @@ public class FormItemController {
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
 
+        // 체크박스, 라디오버튼, 텍스트상자 설정 코드 -- start
         Map<String, String> regions = new LinkedHashMap<>();
         regions.put("SEOUL", "서울");
         regions.put("BUSAN", "부산");
@@ -73,16 +74,69 @@ public class FormItemController {
         deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
         deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
         model.addAttribute("deliveryCodes", deliveryCodes);
+        // 체크박스, 라디오버튼, 텍스트상자 설정 코드 -- end
 
         return "form/addForm";
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String addItem(@ModelAttribute Item item,
+                          RedirectAttributes redirectAttributes,
+                          Model model) {
+
+        //검증 오류 결과를 보관
+        Map<String, String> errors = new HashMap<>();
+
+        //검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            errors.put("price", "가격은 1,000원 ~ 1,000,000 까지 허용합니다.");
+        }
+        if (item.getQuantity() == null || item.getQuantity() > 9999) {
+            errors.put("quantity", "수량은 최대 9,999까지 허용합니다.");
+        }
+
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다.");
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로 이동
+        if (!errors.isEmpty()) {
+            log.info("errors = {}", errors);
+            model.addAttribute("errors", errors);
+
+            // 체크박스, 라디오버튼, 텍스트상자 설정 코드 -- start
+            Map<String, String> regions = new LinkedHashMap<>();
+            regions.put("SEOUL", "서울");
+            regions.put("BUSAN", "부산");
+            regions.put("JEJU", "제주");
+            model.addAttribute("regions",regions);
+
+            ItemType[] values = ItemType.values();
+            model.addAttribute("itemTypes", values);
+
+            List<DeliveryCode> deliveryCodes = new ArrayList<>();
+            deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+            deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+            deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+            model.addAttribute("deliveryCodes", deliveryCodes);
+            // 체크박스, 라디오버튼, 텍스트상자 설정 코드 -- end
+
+            return "form/addForm";
+        }
+
         log.info("item.open={}", item.getOpen());
         log.info("item.regions={}", item.getRegions());
         log.info("item.itemType={}", item.getItemType());
         Item savedItem = itemRepository.save(item);
+
+        // 리다이렉트 주소 매개변수로 사용, status 는 쿼리로 들어감
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
         return "redirect:/form/items/{itemId}";
@@ -93,6 +147,7 @@ public class FormItemController {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
 
+        // 체크박스, 라디오버튼, 텍스트상자 설정 코드 -- start
         Map<String, String> regions = new LinkedHashMap<>();
         regions.put("SEOUL", "서울");
         regions.put("BUSAN", "부산");
@@ -107,6 +162,7 @@ public class FormItemController {
         deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
         deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
         model.addAttribute("deliveryCodes", deliveryCodes);
+        // 체크박스, 라디오버튼, 텍스트상자 설정 코드 -- end
 
         return "form/editForm";
     }
