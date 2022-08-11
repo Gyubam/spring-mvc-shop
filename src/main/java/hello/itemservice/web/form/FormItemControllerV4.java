f7d7ede8dbfa8edaa2c1c45946624222199cbb7b
서규범 <sgb8170@naver.com>
2022-08-12 04:08:53 +0900

    Form 전송 객체 분리

--- src/main/java/hello/itemservice/web/form/FormItemControllerV4.java
+++ src/main/java/hello/itemservice/web/form/FormItemControllerV4.java
@@ -130,15 +130,16 @@ public class FormItemControllerV4 {
 //    }
 
 
+    // 객체명으로 @ModelAttribute 에 입력되기때문에 "item"으로 설정
     @PostMapping("/add")
-    public String addItemv4(@Validated(SaveCheck.class) @ModelAttribute Item item,
+    public String addItem(@Validated @ModelAttribute("item") ItemSaveForm form,
                           BindingResult bindingResult,
                           RedirectAttributes redirectAttributes,
                           Model model) {
 
         //특정 필드가 아닌 복합 룰 검증
-        if (item.getPrice() != null && item.getQuantity() != null) {
-            int resultPrice = item.getPrice() * item.getQuantity();
+        if (form.getPrice() != null && form.getQuantity() != null) {
+            int resultPrice = form.getPrice() * form.getQuantity();
             if (resultPrice < 10000) {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
@@ -168,9 +169,12 @@ public class FormItemControllerV4 {
             return "form/v4/addForm";
         }
 
-        log.info("item.open={}", item.getOpen());
-        log.info("item.regions={}", item.getRegions());
-        log.info("item.itemType={}", item.getItemType());
+        //성공 로직
+        Item item = new Item();
+        item.setItemName(form.getItemName());
+        item.setPrice(form.getPrice());
+        item.setQuantity(form.getQuantity());
+
         Item savedItem = itemRepository.save(item);
 
         // 리다이렉트 주소 매개변수로 사용, status 는 쿼리로 들어감
@@ -206,14 +210,14 @@ public class FormItemControllerV4 {
 
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId,
-                       @Validated(UpdateCheck.class) @ModelAttribute Item item,
+                       @Validated @ModelAttribute("item") ItemUpdateForm form,
                        BindingResult bindingResult,
                        Model model) {
 
 
         //특정 필드가 아닌 복합 룰 검증
-        if (item.getPrice() != null && item.getQuantity() != null) {
-            int resultPrice = item.getPrice() * item.getQuantity();
+        if (form.getPrice() != null && form.getQuantity() != null) {
+            int resultPrice = form.getPrice() * form.getQuantity();
             if (resultPrice < 10000) {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
@@ -243,7 +247,12 @@ public class FormItemControllerV4 {
             return "form/v4/editForm";
         }
 
-        itemRepository.update(itemId, item);
+        Item itemParam = new Item();
+        itemParam.setItemName(form.getItemName());
+        itemParam.setPrice(form.getPrice());
+        itemParam.setQuantity(form.getQuantity());
+
+        itemRepository.update(itemId, itemParam);
 
         return "redirect:/form/v4/items/{itemId}";
     }
